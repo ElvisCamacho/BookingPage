60814d78463041d3b8ef16643b9d45839ebe6321
camachocv@hotmail.com <elvi0506@edu.easj.dk>
2022-01-09 20:06:21 +0100

    last update

---
+++ insert_Data.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT [dbo].[booking] ON
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (2322, N'Muhanad', 12345678, N'muhanad@yahoo.com', N'1988-02-12 00:00:00', N'akshj cool')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (73202, N'Ozzy', 87654321, N'camachocv@hotmail.com', N'2021-01-12 00:00:00', N'ndndnd dnd ')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (72033, N'Elvis', 12345822, N'camachocv@hotmail.com', N'2021-01-12 00:00:00', N'noknok dnd ')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (832202, N'Elvis Camacho', 49409909, N'camachocv@hotmail.com', N'2021-12-16 00:00:00', N'sss')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (23222, N'Muhanad', 12345678, N'muhanad@yahoo.com', N'1988-02-12 00:00:00', N'akshj cool')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (732202, N'Ozzy', 87654321, N'camachocv@hotmail.com', N'2021-01-12 00:00:00', N'ndndnd dnd ')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (722033, N'Elvis', 12345822, N'camachocv@hotmail.com', N'2021-01-12 00:00:00', N'noknok dnd ')
+INSERT INTO [dbo].[booking] ([Id], [Name], [Telephone], [Email], [Date], [Note]) VALUES (832202, N'Elvis Camacho', 49409909, N'camachocv@hotmail.com', N'2021-12-16 00:00:00', N'sss')
+SET IDENTITY_INSERT [dbo].[booking] OFF
